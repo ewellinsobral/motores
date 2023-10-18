39341345b3cf1c6456100db0489c88cf3e3c8fe6
ewellinsobral <ewellin.sobral@escolar.ifrn.edu.br>
2023-10-18 10:16:48 -0300

    motores 8/10

---
+++ projeto/Assets/scripts/Moeda.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moeda : MonoBehaviour
+{
+    public int velocidadeGiro = 150;
+   
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward* velocidadeGiro* Time.deltaTime);
+    }
+}
